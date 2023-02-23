4cb92f085ea67698878fd66315b353afb09da57a
Carolina <87611517+carolinatatiani@users.noreply.github.com>
2023-02-23 22:21:38 +0000

    Pending changes exported from your codespace

--- kwitter.js
+++ kwitter.js
@@ -1,15 +1,16 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBAcdZ1hlUZRzdj7Qxca-9mr9Yldg4-GYE",
-  authDomain: "aula93-7aeb0.firebaseapp.com",
-  databaseURL: "https://aula93-7aeb0-default-rtdb.firebaseio.com",
-  projectId: "aula93-7aeb0",
-  storageBucket: "aula93-7aeb0.appspot.com",
-  messagingSenderId: "832150375879",
-  appId: "1:832150375879:web:e9a17cdaad596f2ce29de2"
+  apiKey: "AIzaSyACsP2QNiw7Pn-DLmp7t14mPABEXXJcLKw",
+  authDomain: "rest-45930.firebaseapp.com",
+  databaseURL: "https://rest-45930-default-rtdb.firebaseio.com",
+  projectId: "rest-45930",
+  storageBucket: "rest-45930.appspot.com",
+  messagingSenderId: "189807751545",
+  appId: "1:189807751545:web:20f669b61805dd63953fa8",
+  measurementId: "G-9G8H2VWVEK"
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-
 
 function addUser() {
   userName = document.getElementById("userName").value;
